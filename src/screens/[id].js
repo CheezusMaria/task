7a0cb91b84b7e7579e85638681cee0118c6041d7
Mustafa Yıldız <38797325+CheezusMaria@users.noreply.github.com>
2023-09-17 19:35:22 +0300

    id.js sayfası veri gösterimi tamamlandı

--- src/screens/[id].js
+++ src/screens/[id].js
@@ -7,76 +7,24 @@ import {
   FlatList,
   ScrollView,
   useWindowDimensions,
-  SafeAreaView,
 } from "react-native";
-import {
-  HTMLElementModel,
-  CustomRendererProps,
-} from "react-native-render-html";
-import HTML from "react-native-render-html";
-import Header from "../../components/Header";
+import RenderHtml from "react-native-render-html";
 import { WebView } from "react-native-webview";
-
+import IframeRenderer, { iframeModel } from "@native-html/iframe-plugin";
 const StartupDetail = ({ route }) => {
   const { height, width, scale, fontScale } = useWindowDimensions();
   const { newsId } = route.params;
   const [New, setNew] = useState({});
-
-  const customHTMLElementModels = {
-    iframe: new HTMLElementModel({
-      tagName: "iframe",
-      mixedUAStyles: { display: "none" }, // iframe processing
-    }),
-  };
-  const renderers = {
-    img: (_htmlAttribs, _children, _convertedCSSStyles, passProps) => {
-      if (_htmlAttribs.src && _htmlAttribs.src.startsWith("http")) {
-        return (
-          <Image
-            source={{ uri: _htmlAttribs.src }}
-            style={{ width: 100, height: 100 }}
-          />
-        );
-      } else {
-        console.log(
-          "Attempted to render image with invalid or non-http src: ",
-          _htmlAttribs.src
-        );
-      }
-      return null;
-    },
-
-    iframe: (_htmlAttribs, _children, _convertedCSSStyles, passProps) => {
-      if (_htmlAttribs.src && _htmlAttribs.src.startsWith("http")) {
-        return (
-          <WebView
-            source={{ uri: _htmlAttribs.src }}
-            style={{ width: 350, height: 200 }}
-          />
-        );
-      } else {
-        console.log(
-          "Attempted to render iframe with invalid or non-http src: ",
-          _htmlAttribs.src
-        );
-      }
-      return null;
-    },
-  };
   useEffect(() => {
     async function fetchData() {
       try {
         const response = await fetch(
-          `https://webrazzi.com/api/v2/posts/${newsId}`,
-          {
-            headers: undefined,
-          }
+          `https://webrazzi.com/api/v2/posts/${newsId}`
         );
 
         if (response.status === 200) {
-          const json = await response.json();
-          console.log(json, "nunu");
-
+          var json = await response.json();
+          json.content = json.content.replaceAll("data-src", "src");
           setNew(json);
         } else {
           setNew({});
@@ -89,15 +37,36 @@ const StartupDetail = ({ route }) => {
     fetchData();
   }, []);
 
+  const renderers = {
+    iframe: IframeRenderer,
+  };
+
+  const customHTMLElementModels = {
+    iframe: iframeModel,
+  };
   return (
-    <SafeAreaView style={{ flex: 1 }}>
-      <Header></Header>
-      <View style={styles.container}>
-        <ScrollView>
+    <View style={styles.container}>
+      <ScrollView style={{ height: height, width: width, padding: 10 }}>
+        <Text
+          style={{
+            textTransform: "uppercase",
+            fontWeight: "bold",
+            marginLeft: 5,
+            color: "gray",
+            marginBottom: 3,
+          }}
+        >
+          {New.categories?.[0]?.title && <Text>{New.categories[0].title}</Text>}
+        </Text>
+        {New.title ? (
+          <Text style={{ fontSize: 28, fontWeight: "bold" }}>{New.title}</Text>
+        ) : null}
+        <View style={{ flex: 1, flexDirection: "row", marginTop: 10 }}>
           <Image
             style={{
-              width: 100,
-              height: 100,
+              width: 40,
+              height: 40,
+              borderRadius: 20,
             }}
             resizeMode="contain"
             source={{
@@ -107,22 +76,38 @@ const StartupDetail = ({ route }) => {
                   : "https://via.placeholder.com/300",
             }}
           />
-          <Text>{New.author && New.author.full_name}</Text>
-          <View style={{ width: width }}>
-            <Text>SA</Text>
-            {/* {New.content && (
-              <HTML
-                customHTMLElementModels={customHTMLElementModels}
-                renderers={renderers}
-                source={{ html: New.content }}
-                contentWidth={width}
-                ignoredDomNode={(domNode) => domNode.name === "iframe"}
-              />
-            )} */}
+          <View
+            style={{
+              flexDirection: "column",
+              justifyContent: "center",
+              paddingLeft: 10,
+            }}
+          >
+            <Text style={{ color: "gray", fontWeight: "800" }}>
+              {New.author && New.author.full_name}
+            </Text>
+            <Text style={{ color: "gray" }}>1 Saat önce</Text>
           </View>
-        </ScrollView>
-      </View>
-    </SafeAreaView>
+        </View>
+        {New.content && (
+          <RenderHtml
+            renderers={renderers}
+            WebView={WebView}
+            contentWidth={width}
+            source={{ html: New.content }}
+            customHTMLElementModels={customHTMLElementModels}
+            renderersProps={{
+              iframe: {
+                scalesPageToFit: true,
+                webViewProps: {
+                  /* Any prop you want to pass to iframe WebViews */
+                },
+              },
+            }}
+          />
+        )}
+      </ScrollView>
+    </View>
   );
 };
 
